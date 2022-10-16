fbced794f6d76e2bed1dec20d855268dadbced58
Barak Yakter <byakter@gmail,com>
2022-10-17 00:48:03 +0300

    New updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,92 @@
-const profileEditButton = document.querySelector('.profile__edit-button');
-const profilePopup = document.querySelector('.popup_type_profile');
- 
-const profileFormElement = document.querySelector('.popup__form_type_profile');
+const profileEditButton = document.querySelector(".profile__edit-button");
+const profilePopup = document.querySelector(".popup_type_profile");
 
-const nameInput = document.querySelector('.popup__input_type_name');
-const jobInput = document.querySelector('.popup__input_type_profession');
+const profileFormElement = document.querySelector(".popup__form_type_profile");
 
-const profileName = document.querySelector('.profile__title');
-const profileJob = document.querySelector('.profile__subtitle');
+const nameInput = document.querySelector(".popup__input_type_name");
+const jobInput = document.querySelector(".popup__input_type_profession");
 
-profileEditButton.addEventListener('click', function(){
-   profilePopup.classList.add('popup_opened')
+const profileName = document.querySelector(".profile__title");
+const profileJob = document.querySelector(".profile__subtitle");
 
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
-})
+const imageCards = document.querySelector(".cards");
+const cardTemplate = document
+  .querySelector(".card-template")
+  .content.querySelector(".cards__card");
+
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
+
+profileEditButton.addEventListener("click", function () {
+  profilePopup.classList.add("popup_opened");
+
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+});
 
 function closePopup(popup) {
-    popup.classList.remove('popup_opened')
+  popup.classList.remove("popup_opened");
 }
- 
-profilePopup.addEventListener('click', function(event){
-    if (event.target.classList.contains('popup') || event.target.classList.contains('popup__close')) {
-     closePopup(profilePopup)
 
-}
-})
+profilePopup.addEventListener("click", function (event) {
+  if (
+    event.target.classList.contains("popup") ||
+    event.target.classList.contains("popup__close")
+  ) {
+    closePopup(profilePopup);
+  }
+});
 
 function handleProfileFormSubmit(evt) {
-   evt.preventDefault();
+  evt.preventDefault();
 
- profileName.textContent = nameInput.value;
- profileJob.textContent = jobInput.value;
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
 
- closePopup(profilePopup)
+  closePopup(profilePopup);
 }
 
-profileFormElement.addEventListener('submit', handleProfileFormSubmit);
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
+
+function generateCard(card) {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector(".cards__title").textContent = card.name;
+  cardElement.querySelector(
+    ".cards__image"
+  ).style.backgroundImage = `url(${card.link})`;
+  return cardElement;
+}
+
+function renderCard(card, container) {
+  container.append(card);
+}
+
+initialCards.forEach(function (card) {
+  const newCard = generateCard(card);
+  renderCard(newCard, imageCards);
+});
